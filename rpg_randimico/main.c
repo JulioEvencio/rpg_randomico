81fc4e176de532b5ad25ac9566f0e62f8ccba46c
JulioEvencio <julio.evencio@acad.ifma.edu.br>
2020-08-12 14:16:37 -0300

    Adicionado estrutura basica em C

---
+++ rpg_randimico/main.c
@@ -0,0 +1,15 @@
+/*  Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/*  Funcoes */
+
+/*  Funcao main */
+int main()
+{
+    /*  */
+
+    return 0;
+}
