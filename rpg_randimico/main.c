9b91115489c47ef2218d699106af24927999ce2b
JulioEvencio <julio.evencio@acad.ifma.edu.br>
2020-08-12 15:40:10 -0300

    Adicionado o ladão e o ogro

--- rpg_randimico/main.c
+++ rpg_randimico/main.c
@@ -4,6 +4,28 @@
 #include <time.h>
 #include <string.h>
 
+/*  Struct dos monstros */
+typedef struct Lista_monstros
+{
+    char nome[100];
+    int ataque;
+    int vida;
+    int velocidade;
+}Base_monstros;
+Base_monstros monstros[2];
+
+/*  Lista de monstros */
+/*  Ladrao */
+strcpy(monstros[0].nome, "Ladrao");
+monstros[0].ataque = 3;
+monstros[0].vida = 3;
+monstros[0].velocidade = 3;
+/*  Ogro */
+strcpy(monstros[1].nome, "Ogro");
+monstros[1].ataque = 2;
+monstros[1].vida = 5;
+monstros[1].velocidade = 1;
+
 /*  Funcoes */
 
 /*  Funcao main */
