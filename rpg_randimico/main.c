18ee9c958db10879f92a2ab5b483a8b6514d6944
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-08-13 19:44:40 -0300

    Adiconado a função do padre

--- rpg_randimico/main.c
+++ rpg_randimico/main.c
@@ -35,3 +35,19 @@ int main()
 
     return 0;
 }
+
+/*  Funcao do padre */
+void acao_padre(void)
+{
+    printf("Padre: ");
+    if(personagem.vida_atual != personagem.vida_maxima)
+    {
+        printf("Vou curar suas feridas! Ate logo bravo aventureiro! \n");
+        personagem.vida_atual = personagem.vida_maxima;
+    }
+    else
+    {
+        printf("Cuidado com os monstros desse mundo! \n");
+    }
+    
+}
