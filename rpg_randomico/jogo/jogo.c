cfff0439d7fb35322b79147312cf1d11e415294c
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-06-08 01:17:14 -0300

    Adicionada função que imprime os dados do jogador na tela

--- rpg_randomico/jogo/jogo.c
+++ rpg_randomico/jogo/jogo.c
@@ -14,15 +14,17 @@ struct Jogo
 };
 typedef struct Jogo Jogo;
 
+void jogo_imprimir_jogador(Jogo *jogo);
+
 int jogo_jogar(void)
 {
     int loop = 1;
     Jogo jogo;
     char nome[JOGADOR_NOME];
 
-    system(LIMPAR_TELA);
     srand(time(NULL));
 
+    system(LIMPAR_TELA);
     puts("----------------- Aventura -----------------");
 
     printf("Digite seu nome de aventureiro(a): ");
@@ -32,6 +34,10 @@ int jogo_jogar(void)
 
     while (loop)
     {
+        system(LIMPAR_TELA);
+        puts("----------------- Aventura -----------------");
+
+        jogo_imprimir_jogador(&jogo);
         loop = 0;
     }
 
@@ -39,4 +45,22 @@ int jogo_jogar(void)
 
     return 0;
 
+}
+
+void jogo_imprimir_jogador(Jogo *jogo)
+{
+    printf("Nome: %s \n", jogador_obter_nome(&jogo->jogador));
+    printf("Ouro: %d \n", jogador_obter_ouro(&jogo->jogador));
+    printf("Vida: %d / %d \n\n", jogador_obter_vida(&jogo->jogador), jogador_obter_vida_max(&jogo->jogador));
+
+    if (jogador_tem_arma(&jogo->jogador)) printf("Arma: Sim \n");
+    else printf("Arma: Nao \n");
+
+    if (jogador_tem_armadura(&jogo->jogador)) printf("Armadura: Sim \n");
+    else printf("Armadura: Nao \n");
+
+    if (jogador_tem_bota(&jogo->jogador)) printf("Bota: Sim \n");
+    else printf("Bota: Nao \n");
+
+    puts("\n--------------------------------------------");
 }
