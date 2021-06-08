dca8101c094db90b3eb5906d7a13d345eccd3838
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-06-08 01:55:04 -0300

    Adicionados códigos que geram a aventura de forma aleatória

--- rpg_randomico/jogo/jogo.c
+++ rpg_randomico/jogo/jogo.c
@@ -6,6 +6,27 @@
 #include "../jogador/jogador.h"
 #include "../inimigo/inimigo.h"
 
+#define JOGO_GERAR_TERRENO rand() % 5
+#define JOGO_GERAR_ENCONTRO rand() % 5
+
+enum Terrenos
+{
+    TERRENO_CIDADE,
+    TERRENO_DESERTO,
+    TERRENO_FLORESTA,
+    TERRENO_PLANICE,
+    TERRENO_MASMORRA
+};
+
+enum Encontros
+{
+    ENCONTRO_ALDEAO,
+    ENCONTRO_COMERCIANTE,
+    ENCONTRO_PADRE,
+    ENCONTRO_INIMIGO,
+    ENCONTRO_NADA
+};
+
 struct Jogo
 {
     int dias;
@@ -19,7 +40,7 @@ void jogo_imprimir_jogador(Jogo *jogo);
 int jogo_jogar(void)
 {
     int loop = 1;
-    Jogo jogo;
+    Jogo jogo = {0, NULL, NULL};
     char nome[JOGADOR_NOME];
 
     srand(time(NULL));
@@ -37,11 +58,69 @@ int jogo_jogar(void)
         system(LIMPAR_TELA);
         puts("----------------- Aventura -----------------");
 
+        jogo.dias++;
+
         jogo_imprimir_jogador(&jogo);
-        loop = 0;
+
+        switch (JOGO_GERAR_TERRENO)
+        {
+            case TERRENO_CIDADE:
+                printf("Voce chegou a uma cidade ");
+                break;
+
+            case TERRENO_DESERTO:
+                printf("Voce chegou a um deserto ");
+                break;
+
+            case TERRENO_FLORESTA:
+                printf("Voce chegou a uma floresta ");
+                break;
+
+            case TERRENO_PLANICE:
+                printf("Voce chegou a uma planice ");
+                break;
+
+            case TERRENO_MASMORRA:
+                printf("Voce chegou a uma masmorra ");
+                break;
+        }
+
+        switch (JOGO_GERAR_ENCONTRO)
+        {
+            case ENCONTRO_ALDEAO:
+                printf("e encontrou um aldeao... \n");
+                /* Funcao do aldeao */
+                break;
+
+            case ENCONTRO_COMERCIANTE:
+                printf("e encontrou um comerciante... \n");
+                /* Funcao do comerciante */
+                break;
+
+            case ENCONTRO_PADRE:
+                printf("e encontrou um padre... \n");
+                /* Funcao do padre */
+                break;
+
+            case ENCONTRO_INIMIGO:
+                printf("e encontrou um inimigo... \n");
+                /* Funcao do inimigo */
+                break;
+
+            case ENCONTRO_NADA:
+                printf("e encontrou nada... \n");
+                break;
+        }
+
+        if (ler_numero(&loop)) loop = -1;
+
+        pausar_tela("Pressione enter para continuar...");
     }
 
     jogador_liberar(&jogo.jogador);
+
+    puts("\n--------------------------------------------");
+    printf("Voce sobreviveu por %d dias \n", jogo.dias);
 
     return 0;
 
@@ -50,6 +129,7 @@ int jogo_jogar(void)
 void jogo_imprimir_jogador(Jogo *jogo)
 {
     printf("Nome: %s \n", jogador_obter_nome(&jogo->jogador));
+    printf("Dia: %d \n", jogo->dias);
     printf("Ouro: %d \n", jogador_obter_ouro(&jogo->jogador));
     printf("Vida: %d / %d \n\n", jogador_obter_vida(&jogo->jogador), jogador_obter_vida_max(&jogo->jogador));
 
